01bb03869a066de0c0885c4230f1aea729dc3b45
huangyueran <huangyueran@163.com>
2017-04-28 20:41:57 +0800

    hadoop+ha+mapreduce+yarn

--- src/hdfs02/Run.java
+++ src/hdfs02/Run.java
@@ -3,6 +3,7 @@ package hdfs02;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
@@ -19,8 +20,11 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Run {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
-		conf.set("fs.default.name", "hdfs://master:9000");
-		Job job = Job.getInstance();
+//		conf.set("fs.default.name", "hdfs://master:8020");
+//		conf.set("yarn.resourcemanager.hostname","master");
+		conf.set("mapred.jar", "H:\\jar\\wc.jar");
+		
+		Job job = Job.getInstance(conf);
 
 		job.setMapperClass(WcMapper.class);
 		job.setReducerClass(WcReduce.class);
@@ -29,11 +33,16 @@ public class Run {
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(LongWritable.class);
 
-		// job.setNumReduceTasks(20); // 设置Reduce任务个数
+		// job.setNumReduceTasks(20); // 设置Reduce任务个数 
 
 		FileInputFormat.setInputPaths(job, "/user/input/wc");
-		FileOutputFormat.setOutputPath(job, new Path("/user/ouput/wc"));
+		FileOutputFormat.setOutputPath(job, new Path("/user/ouput/wc2"));
 
+		FileSystem fs = FileSystem.get(conf);
+		if(fs.exists(new Path("/user/ouput/wc2"))){
+			fs.delete(new Path("/user/ouput/wc2"),true);
+		}
+		
 		job.waitForCompletion(true); // 是否等待完成
 
 	}
--- src/hdfs02/qq/friend/Run.java
+++ src/hdfs02/qq/friend/Run.java
@@ -3,6 +3,7 @@ package hdfs02.qq.friend;
 import java.io.IOException;
 
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
@@ -17,7 +18,7 @@ import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 public class Run {
 	public static void main(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
 		Configuration conf = new Configuration();
-		conf.set("fs.default.name", "hdfs://master:9000");
+		conf.set("fs.default.name", "hdfs://master:8020");
 		Job job = Job.getInstance();
 
 		job.setJobName("qq");
@@ -32,6 +33,11 @@ public class Run {
 
 		FileInputFormat.setInputPaths(job, "/user/input/qq");
 		FileOutputFormat.setOutputPath(job, new Path("/user/ouput/qq"));
+		
+		FileSystem fs = FileSystem.get(conf);
+		if(fs.exists(new Path("/user/ouput/qq"))){
+			fs.delete(new Path("/user/ouput/qq"),true);
+		}
 
 		job.waitForCompletion(true); // 是否等待完成
 
